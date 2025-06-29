35847ba819023cdeed834087bf82d5b9ae6bdc45
andikadwikid <andika0106dwiki@gmail.com>
2025-06-29 22:21:31 +0700

    first commit

---
+++ app/Models/HomeService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Str;
+
+class HomeService extends Model
+{
+    use SoftDeletes;
+    protected $guarded = [];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function benefits(): HasMany
+    {
+        return $this->hasMany(ServiceBenefit::class);
+    }
+
+    public function testimonials(): HasMany
+    {
+        return $this->hasMany(ServiceTestimonial::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
